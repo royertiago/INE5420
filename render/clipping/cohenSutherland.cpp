86bcc25c2c0a301f28a7c288c343afcba242562c
Tiago Royer <royertiago@gmail.com>
2014-04-24 23:33:59 -0300

    Algoritmo não funcionando, primeira tentativa...

--- render/clipping/cohenSutherland.cpp
+++ render/clipping/cohenSutherland.cpp
@@ -2,7 +2,12 @@
  * Implementação de cohenSutherland.h
  */
 
+#include <cmath>
+using std::fabs;
+
 #include "cohenSutherland.h"
+
+const double epsilon = 1e-10;
 
 CohenSutherland::CohenSutherland( ClippingArea a ): 
     clippingArea(a)
@@ -15,12 +20,32 @@ void CohenSutherland::setArea( ClippingArea a ) {
 
 bool CohenSutherland::clip( Math::Point<2>& p1, Math::Point<2>& p2 )
 {
-    if(regionOf(p1) == MIDDLE && regionOf(p2) == MIDDLE)
+    int r1 = regionOf(p1);
+    int r2 = regionOf(p2);
+    if(r1 == MIDDLE && r2 == MIDDLE)
+        // Caso trivial; nenhum clipping a ser feito.
         return true;
-    else
-    {   
+
+    if(r1 & r2)
+        /* Region codes: se o E lógico entre dois region codes
+         * for não-zero, significa que o ponto está totalmente
+         * fora da área de desenho. Não-zero ~= true. */
         return false;
+
+    /* Primeiro caso patológico: um ponto está acima do outro, 
+     * formando uma reta vertical, não uma função afim. */
+    if( fabs( p1[0] == p2[0] ) < epsilon ) {
+        if( p1[0] < a.xmin || p1[0] > a.xmax ) // fora da área de clipping
+            return false;
+        p1[1] = fmax( p[1], a.ymax );
+        p1[1] = fmin( p[1], a.ymin );
+        p2[1] = fmax( p[1], a.ymax );
+        p2[1] = fmin( p[1], a.ymin ); // Aparar por cima e por baixo
+        return true;
     }
+
+    double c = (p1[1] - p2[1])/(p1[0] - p2[0]); //Coeficiente angular
+    return true;
 }
 
 int CohenSutherland::regionOf( Math::Point<2>& p )
