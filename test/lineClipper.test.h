3758cb0e0eccd262ab6e58a7068cbfbc43336c2e
Tiago Royer <royertiago@gmail.com>
2014-04-24 22:43:04 -0300

    Adicionados mais três testes ao teste de clipping de linhas.

--- test/lineClipper.test.h
+++ test/lineClipper.test.h
@@ -38,10 +38,35 @@ inline bool testLineClipper( LineClipper<2> * clipper, int precision ) {
     x = Math::Point<2>{12, 12};
     y = Math::Point<2>{17, 17};
     b &= Test::TEST_EQUALS( clipper->clip( x, y ), true );
+    b &= d.TEST_EQUALS( x[0], 12.0 );
+    b &= d.TEST_EQUALS( x[1], 12.0 );
+    b &= d.TEST_EQUALS( y[0], 17.0 );
+    b &= d.TEST_EQUALS( y[1], 17.0 );
 
-    // Reta completamente fora da área de clipping
+    // Reta completamente fora da área de clipping, a sudoeste.
     x = Math::Point<2>{5, 5};
     y = Math::Point<2>{5, 8};
+    b &= Test::TEST_EQUALS( clipper->clip( x, y ), false );
+
+    // Reta completamente fora da área de clipping, ao sul.
+    x = Math::Point<2>{5, 5};
+    y = Math::Point<2>{5, 35};
+    b &= Test::TEST_EQUALS( clipper->clip( x, y ), false );
+
+    /* Reta parcialmente dentro da área de clipping, com um ponto
+     * a sudoeste e um a nordeste da área de clipping. */
+    x = Math::Point<2>{0, 0};
+    y = Math::Point<2>{30, 30};
+    b &= Test::TEST_EQUALS( clipper->clip( x, y ), true );
+    b &= d.TEST_EQUALS( x[0], 10.0 );
+    b &= d.TEST_EQUALS( x[1], 10.0 );
+    b &= d.TEST_EQUALS( y[0], 20.0 );
+    b &= d.TEST_EQUALS( y[1], 20.0 );
+
+    /* Reta totalmente fora da área de clipping, mas com um ponto
+     * a sudoeste e um a nordeste da área de clipping. */
+    x = Math::Point<2>{-40, 0};
+    y = Math::Point<2>{30, 70};
     b &= Test::TEST_EQUALS( clipper->clip( x, y ), false );
 
     return b;
